600b18aada0907768167e1c5d797ad3c1406b249
ollivier remy <remy.olliver@gmail.com>
2023-11-22 11:39:05 +0100

    Fix useless lines

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 const { Client, GatewayIntentBits, Collection} = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes} = require('discord-api-types/v9');
 const dotenv = require('dotenv');
 const fs = require('fs');
 
 dotenv.config();
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (let file of commandFiles) {
-    let command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
-
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
-
-(async() => {
-    try {
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands },
-        );
-
-        console.log('Successfully registered application commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 client.commands = new Collection();
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
